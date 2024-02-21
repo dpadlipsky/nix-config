80110cf59e0df1cd034f458d9adc428e96ec0406
David Padlipsky <david.padlipsky@gmail.com>
2024-02-21 02:53:45 -0800

    Minor kitty config additions

--- modules/home-manager/terminals/kitty.nix
+++ modules/home-manager/terminals/kitty.nix
@@ -8,6 +8,8 @@
     };
     settings = {
       background_opacity = "0.90";
+      scrollback_lines = 10000;
+      mouse_map = "ctrl+left click ungrabbed mouse_handle_click selection link prompt";
     };
     shellIntegration.enableBashIntegration = true;
     theme = "Tokyo Night";

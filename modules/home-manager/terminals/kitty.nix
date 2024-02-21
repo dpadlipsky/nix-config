6058303defc26cceacee3c96bc4b304b1f19aff6
David Padlipsky <david.padlipsky@gmail.com>
2024-02-21 02:48:46 -0800

    Add basic kitty configuration

--- modules/home-manager/terminals/kitty.nix
+++ modules/home-manager/terminals/kitty.nix
@@ -1,7 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, programs, ... }:
 
 {
-  home.packages = [
-    pkgs.kitty
-  ];
+  programs.kitty = {
+    enable = true;
+    keybindings = {
+      "ctrl+c" = "copy_or_interrupt";
+    };
+    settings = {
+      background_opacity = "0.90";
+    };
+    shellIntegration.enableBashIntegration = true;
+    theme = "Tokyo Night";
+  };
 }
